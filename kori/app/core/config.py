bd3921283037e178738d029372559a326300bb51
Sheerabth <sheerabth1610@gmail.com>
2023-03-07 08:44:35 +0530

    change: Optional[type] -> type | None

--- kori/app/core/config.py
+++ kori/app/core/config.py
@@ -1,4 +1,4 @@
-from typing import Any, Optional
+from typing import Any
 
 from dotenv import find_dotenv
 from pydantic import BaseSettings, PostgresDsn, validator
@@ -18,11 +18,11 @@ class Settings(BaseSettings):
     DATABASE_USER: str
     DATABASE_PASSWORD: str
     DATABASE_NAME: str
-    DATABASE_URI: Optional[str] = None
+    DATABASE_URI: str | None = None
 
     @validator("DATABASE_URI")
     def construct_database_connection_uri(
-        cls, v: Optional[str], values: dict[str, Any]
+        cls, v: str | None, values: dict[str, Any]
     ) -> str | PostgresDsn:
         if isinstance(v, str):
             return v
