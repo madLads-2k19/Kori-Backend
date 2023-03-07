1b3570e15162481e1aa085bc76bd66394f6ebecb
Sheerabth <sheerabth1610@gmail.com>
2023-03-07 08:33:03 +0530

    chore: optimal dict comprehension

--- kori/app/utils/dict_utils.py
+++ kori/app/utils/dict_utils.py
@@ -2,4 +2,4 @@ from typing import Any
 
 
 def remove_null_values(dictionary: dict[Any, Any]) -> dict[Any, Any]:
-    return dict([(k, v) for k, v in dictionary.items() if v is not None])
+    return {k: v for k, v in dictionary.items() if v is not None}
