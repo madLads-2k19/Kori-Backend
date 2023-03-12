a34c285b0f6445e9d94061b8938533a663ec8490
Sheerabth <sheerabth1610@gmail.com>
2023-03-12 19:39:07 +0530

    change: pydantic models -> schema

--- kori/app/dao/customer.py
+++ kori/app/dao/customer.py
@@ -5,8 +5,8 @@ from sqlalchemy.exc import IntegrityError
 from kori.app.core.config import Settings
 from kori.app.core.exceptions import DuplicateRecordException
 from kori.app.db.connection import DbConnector
-from kori.app.models.customer import CustomerOrm
-from kori.app.schemas.customer import Customer, CustomerCreate, CustomerUpdate
+from kori.app.models.customer import Customer
+from kori.app.schemas.customer import CustomerCreate, CustomerSchema, CustomerUpdate
 from kori.app.utils.dict_utils import remove_null_values
 
 settings = Settings()
@@ -14,9 +14,9 @@ settings = Settings()
 db_connector = DbConnector(settings.DATABASE_URI)
 
 
-def create(customer_data: CustomerCreate) -> Customer:
+def create(customer_data: CustomerCreate) -> CustomerSchema:
     session = db_connector.get_session()
-    new_customer_db = CustomerOrm(**customer_data.dict())
+    new_customer_db = Customer(**customer_data.dict())
 
     try:
         session.add(new_customer_db)
@@ -24,27 +24,27 @@ def create(customer_data: CustomerCreate) -> Customer:
     except IntegrityError:
         raise DuplicateRecordException(message="Account with similar phone number already exists.")
 
-    return Customer.from_orm(new_customer_db)
+    return CustomerSchema.from_orm(new_customer_db)
 
 
-def get_customer_by_id(customer_id: UUID) -> Customer | None:
+def get_customer_by_id(customer_id: UUID) -> CustomerSchema | None:
     session = db_connector.get_session()
-    customers = list(session.query(CustomerOrm).filter(CustomerOrm.id == customer_id))
-    return Customer.from_orm(customers[0]) if customers else None
+    customers = list(session.query(Customer).filter(Customer.id == customer_id))
+    return CustomerSchema.from_orm(customers[0]) if customers else None
 
 
-def get_customer_by_number(phone_number: str) -> Customer | None:
+def get_customer_by_number(phone_number: str) -> CustomerSchema | None:
     session = db_connector.get_session()
-    customers = list(session.query(CustomerOrm).filter(CustomerOrm.phone_number == phone_number))
-    return Customer.from_orm(customers[0]) if customers else None
+    customers = list(session.query(Customer).filter(Customer.phone_number == phone_number))
+    return CustomerSchema.from_orm(customers[0]) if customers else None
 
 
-def update(customer_id: UUID, customer_data: CustomerUpdate) -> Customer:
+def update(customer_id: UUID, customer_data: CustomerUpdate) -> CustomerSchema:
     session = db_connector.get_session()
     update_data = remove_null_values(customer_data.dict())
 
     try:
-        session.query(CustomerOrm).filter(CustomerOrm.id == customer_id).update(update_data)
+        session.query(Customer).filter(Customer.id == customer_id).update(update_data)
         session.commit()
     except IntegrityError:
         raise DuplicateRecordException(message="Account with similar phone number already exists.")
@@ -52,7 +52,7 @@ def update(customer_id: UUID, customer_data: CustomerUpdate) -> Customer:
     return get_customer_by_id(customer_id)
 
 
-def delete(phone_number: str) -> None:
+def delete(customer_id: UUID) -> None:
     session = db_connector.get_session()
-    session.query(CustomerOrm).filter(CustomerOrm.phone_number == phone_number).delete()
+    session.query(Customer).filter(Customer.id == customer_id).delete()
     session.commit()
--- kori/app/services/customer.py
+++ kori/app/services/customer.py
@@ -1,24 +1,25 @@
 from uuid import UUID
 
 import kori.app.dao.customer as customer_dao
-from kori.app.schemas.customer import Customer, CustomerCreate, CustomerUpdate
+from kori.app.schemas.customer import CustomerCreate, CustomerCreateRequest, CustomerSchema, CustomerUpdate
 
 
-def create(customer_data: CustomerCreate) -> Customer:
-    return customer_dao.create(customer_data)
+def create(org_id: UUID, customer_data: CustomerCreateRequest) -> CustomerSchema:
+    customer_create = CustomerCreate(org_id=org_id, **customer_data.dict())
+    return customer_dao.create(customer_create)
 
 
-def get_customer_by_id(customer_id: UUID) -> Customer | None:
+def get_customer_by_id(customer_id: UUID) -> CustomerSchema | None:
     return customer_dao.get_customer_by_id(customer_id)
 
 
-def get_customer_by_number(phone_number: str) -> Customer | None:
+def get_customer_by_number(phone_number: str) -> CustomerSchema | None:
     return customer_dao.get_customer_by_number(phone_number)
 
 
-def update(customer_id: UUID, customer_data: CustomerUpdate) -> Customer:
+def update(customer_id: UUID, customer_data: CustomerUpdate) -> CustomerSchema:
     return customer_dao.update(customer_id, customer_data)
 
 
-def delete(phone_number: str) -> None:
-    return customer_dao.delete(phone_number)
+def delete(customer_id: UUID) -> None:
+    return customer_dao.delete(customer_id)
