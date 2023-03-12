ae72d10a942c81566f34d97bf02cfaae9faf6c64
saketh <sakethramansundaram78@gmail.com>
2023-03-12 22:10:16 +0530

    fix: handle exceptions

--- kori/app/dao/organization.py
+++ kori/app/dao/organization.py
@@ -1,6 +1,7 @@
 from uuid import UUID
 
 from kori.app.core.config import Settings
+from kori.app.core.exceptions import NotFoundException
 from kori.app.db.connection import DbConnector
 from kori.app.models.organization import Organization
 from kori.app.schemas.organization import OrganizationCreate, OrganizationSchema, OrganizationUpdate
@@ -21,9 +22,13 @@ def create(organization_create: OrganizationCreate) -> OrganizationSchema:
     return OrganizationSchema.from_orm(new_org_db)
 
 
-def get_organization_by_id(organization_id: UUID) -> OrganizationSchema | None:
+def get_organization_by_id(organization_id: UUID) -> OrganizationSchema:
     session = db_connector.get_session()
     organization = session.query(Organization).get(organization_id)
+
+    if organization is None:
+        raise NotFoundException()
+
     return OrganizationSchema.from_orm(organization)
 
 
@@ -37,14 +42,19 @@ def update(organization_id: UUID, organization_update: OrganizationUpdate) -> Or
     session = db_connector.get_session()
     update_data = remove_null_values(organization_update.dict())
 
-    session.query(Organization).filter(Organization.id == organization_id).update(update_data)
-
+    updated_count = session.query(Organization).filter(Organization.id == organization_id).update(update_data)
     session.commit()
+
+    if updated_count == 0:
+        raise NotFoundException(message="No records matched for update")
 
     return get_organization_by_id(organization_id)
 
 
 def delete(organization_id: UUID) -> None:
     session = db_connector.get_session()
-    session.query(Organization).filter(Organization.id == organization_id).delete()
+    deleted_count = session.query(Organization).filter(Organization.id == organization_id).delete()
     session.commit()
+
+    if deleted_count == 0:
+        raise NotFoundException(message="No records matched for delete")
