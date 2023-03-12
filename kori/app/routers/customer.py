ff181becc55c825907db904669316f9fbee5bba7
Sheerabth <sheerabth1610@gmail.com>
2023-03-12 19:39:07 +0530

    change: pydantic models -> schema

--- kori/app/routers/customer.py
+++ kori/app/routers/customer.py
@@ -4,76 +4,33 @@ from fastapi import APIRouter
 
 import kori.app.services.customer as customer_service
 from kori.app.core.config import Settings
-from kori.app.schemas.customer import Customer, CustomerCreate, CustomerUpdate
+from kori.app.schemas.customer import CustomerCreateRequest, CustomerSchema, CustomerUpdate
 
 customer_router = APIRouter()
 settings = Settings()
 
 
-@customer_router.post("/", response_model=Customer)
-def register_customer(customer_data: CustomerCreate) -> Customer:
-    return customer_service.create(customer_data)
-    # return Customer(
-    #     id=uuid4(),
-    #     **customer_data.dict(),
-    # )
+@customer_router.post("/{organization_id}", response_model=CustomerSchema)
+def register_customer(organization_id: UUID, customer_data: CustomerCreateRequest) -> CustomerSchema:
+    return customer_service.create(organization_id, customer_data)
 
 
-@customer_router.get("/{customer_id}", response_model=Customer)
-def get_customer_by_id(customer_id: UUID) -> Customer:
+@customer_router.get("/{customer_id}", response_model=CustomerSchema)
+def get_customer_by_id(customer_id: UUID) -> CustomerSchema:
     return customer_service.get_customer_by_id(customer_id)
-    # return Customer.parse_obj(
-    #     {
-    #         "org_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-    #         "email": "user@example.com",
-    #         "is_member": False,
-    #         "membership_points": 0,
-    #         "address": "string",
-    #         "preferred_payment_method": "string",
-    #         "name": "string",
-    #         "phone_number": "string",
-    #         "id": "b6d47eb3-2bd2-4f8a-b565-e4ea162f965d"
-    #     }
-    # )
 
 
-@customer_router.get("/{phone_number}", response_model=Customer)
-def get_customer_by_phone_number(phone_number: str) -> Customer:
+@customer_router.get("/number/{phone_number}", response_model=CustomerSchema)
+def get_customer_by_phone_number(phone_number: str) -> CustomerSchema:
     return customer_service.get_customer_by_number(phone_number)
-    # return Customer.parse_obj(
-    #     {
-    #         "org_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-    #         "email": "user@example.com",
-    #         "is_member": False,
-    #         "membership_points": 0,
-    #         "address": "string",
-    #         "preferred_payment_method": "string",
-    #         "name": "string",
-    #         "phone_number": "string",
-    #         "id": "b6d47eb3-2bd2-4f8a-b565-e4ea162f965d"
-    #     }
-    # )
 
 
-@customer_router.put("/{customer_id}", response_model=Customer)
-def update_customer(customer_id: UUID, customer_data: CustomerUpdate) -> Customer:
+@customer_router.put("/{customer_id}", response_model=CustomerSchema)
+def update_customer(customer_id: UUID, customer_data: CustomerUpdate) -> CustomerSchema:
     return customer_service.update(customer_id, customer_data)
-    # return Customer.parse_obj(
-    #     {
-    #         "org_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-    #         "email": "user@example.com",
-    #         "is_member": False,
-    #         "membership_points": 0,
-    #         "address": "string",
-    #         "preferred_payment_method": "string",
-    #         "name": "string",
-    #         "phone_number": "string",
-    #         "id": "b6d47eb3-2bd2-4f8a-b565-e4ea162f965d"
-    #     }
-    # )
 
 
-@customer_router.delete("/{phone_number}")
-def delete_customer(phone_number: str):
-    customer_service.delete(phone_number)
+@customer_router.delete("/{customer_id}")
+def delete_customer(customer_id: UUID):
+    customer_service.delete(customer_id)
     return "Deleted"
--- kori/app/schemas/customer.py
+++ kori/app/schemas/customer.py
@@ -1,10 +1,10 @@
+import re
 from uuid import UUID
 
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, validator
 
 
 class CustomerBase(BaseModel):
-    org_id: UUID
     email: EmailStr | None = None
     is_member: bool = False
     membership_points: int = 0
@@ -12,17 +12,37 @@ class CustomerBase(BaseModel):
     preferred_payment_method: str | None = None
 
 
-class CustomerCreate(CustomerBase):
+class CustomerCreateRequest(CustomerBase):
     name: str
     phone_number: str
+
+    @validator("phone_number")
+    def validate_phone_number(cls, value: str) -> str:
+        match = re.search(r"^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$", value)
+        if not match:
+            raise ValueError("Invalid phone number")
+        return value
 
 
 class CustomerUpdate(CustomerBase):
     name: str | None = None
     phone_number: str | None = None
 
+    @validator("phone_number")
+    def validate_phone_number(cls, value: str | None) -> str | None:
+        if not value:
+            return
+        match = re.search(r"^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$", value)
+        if not match:
+            raise ValueError("Invalid phone number")
+        return value
 
-class Customer(CustomerCreate):
+
+class CustomerCreate(CustomerCreateRequest):
+    org_id: UUID
+
+
+class CustomerSchema(CustomerCreate):
     id: UUID
 
     class Config:
