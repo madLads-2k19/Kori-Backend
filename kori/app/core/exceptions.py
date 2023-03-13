773b6b0d1de9f2a1f0f1c5f3c9d313790a6d9aa3
baxx <susanth1503@gmail.com>
2023-03-13 14:16:44 +0530

    Add: doa for products route Add: Bad request Exception Add: Schemas for Product

--- kori/app/core/exceptions.py
+++ kori/app/core/exceptions.py
@@ -6,28 +6,37 @@ class KoriException(HTTPException):
         super(HTTPException, self).__init__(status_code, message)
 
 
-class DuplicateRecordException(KoriException):
+class BadRequestException(KoriException):
     def __init__(
-        self,
-        status_code: int = status.HTTP_409_CONFLICT,
-        message: str = "Record already exists",
-    ):
-        super(KoriException, self).__init__(status_code, message)
-
-
-class NotFoundException(KoriException):
-    def __init__(
-        self,
-        status_code: int = status.HTTP_404_NOT_FOUND,
-        message: str = "Record not found",
+            self,
+            status_code: int = status.HTTP_400_BAD_REQUEST,
+            message: str = "Bad Request",
     ):
         super(KoriException, self).__init__(status_code, message)
 
 
 class ForbiddenException(KoriException):
     def __init__(
-        self,
-        status_code: int = status.HTTP_403_FORBIDDEN,
-        message: str = "Access forbidden",
+            self,
+            status_code: int = status.HTTP_403_FORBIDDEN,
+            message: str = "Access forbidden",
+    ):
+        super(KoriException, self).__init__(status_code, message)
+
+
+class NotFoundException(KoriException):
+    def __init__(
+            self,
+            status_code: int = status.HTTP_404_NOT_FOUND,
+            message: str = "Record not found",
+    ):
+        super(KoriException, self).__init__(status_code, message)
+
+
+class DuplicateRecordException(KoriException):
+    def __init__(
+            self,
+            status_code: int = status.HTTP_409_CONFLICT,
+            message: str = "Record already exists",
     ):
         super(KoriException, self).__init__(status_code, message)
