d216a2332030dbea4a350d0ed9a9c79c0f9364ad
saketh <sakethramansundaram78@gmail.com>
2023-03-13 11:25:33 +0530

    add: APIs for Store CRUD

--- kori/__main__.py
+++ kori/__main__.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 from kori.app.core.config import Settings
 from kori.app.routers.customer import customer_router
 from kori.app.routers.organization import organization_router
+from kori.app.routers.store import store_router
 
 settings = Settings()
 
@@ -20,6 +21,13 @@ app.include_router(
     prefix=f"/organization/v1",
     tags=["Organization API V1"],
 )
+
+app.include_router(
+    store_router,
+    prefix=f"/store/v1",
+    tags=["Store API V1"],
+)
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host=settings.SERVER_HOST, port=settings.SERVER_PORT)
