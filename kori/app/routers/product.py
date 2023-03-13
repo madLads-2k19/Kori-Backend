84f963b922547b39d51252230681226149cbdec7
baxx <susanth1503@gmail.com>
2023-03-13 14:17:11 +0530

    Add: Routes and Services for product API

---
+++ kori/app/routers/product.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+from typing import Optional
+from uuid import UUID
+from fastapi import APIRouter
+
+from kori.app.core.config import Settings
+import kori.app.services.product as product_service
+from kori.app.schemas.product import ProductCreateRequest, ProductSchema, ProductUpdate
+
+product_router = APIRouter()
+settings = Settings()
+
+
+@product_router.post("/{org_id}", response_model=ProductSchema)
+def create_product(org_id: UUID, product_data: ProductCreateRequest) -> ProductSchema:
+    return product_service.create(org_id=org_id, product_data=product_data)
+
+
+@product_router.get("/{product_id}", response_model=ProductSchema)
+def get_product_by_id(product_id: UUID, timestamp: Optional[datetime] = None) -> ProductSchema:
+    if timestamp:
+        return product_service.get_product_by_timestamp(product_id=product_id, timestamp=timestamp)
+    else:
+        return product_service.get_latest_product_by_id(product_id=product_id)
+
+
+@product_router.put("/{product_id}", response_model=ProductSchema)
+def update_product(product_id: UUID, product_data: ProductUpdate) -> ProductSchema:
+    return product_service.update(product_id=product_id, product_data=product_data)
+
+
+@product_router.delete("/{product_id}")
+def delete_product(product_id: UUID):
+    product_service.delete(product_id)
+    return "Deleted"
