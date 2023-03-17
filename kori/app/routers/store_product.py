55437b55efa970aa8dcb3854a76a1cc9d8715217
saketh <sakethramansundaram78@gmail.com>
2023-03-17 20:37:05 +0530

    add: API to fetch store products across store for a given product

--- kori/app/routers/store_product.py
+++ kori/app/routers/store_product.py
@@ -26,7 +26,7 @@ def create_store_product_mapping(organization_id: UUID, store_product_create: St
     return store_product_dao.create(store_product_create)
 
 
-@store_product_router.get(BASE + "/{store_id}/{product_id}", response_model=StoreProductSchema)
+@store_product_router.get(BASE + "/store/{store_id}/product/{product_id}", response_model=StoreProductSchema)
 def get_store_product(organization_id: UUID, store_id: UUID, product_id: UUID) -> StoreProductSchema:
     store = store_dao.get_store_by_id(organization_id, store_id)
     if store is None:
@@ -35,9 +35,22 @@ def get_store_product(organization_id: UUID, store_id: UUID, product_id: UUID) -
     return store_product_dao.get_store_product(store_id, product_id)
 
 
-@store_product_router.get(BASE + "/{store_id}", response_model=list[StoreProductSchema])
-def get_all_store_products(organization_id: UUID, store_id: UUID) -> list[StoreProductSchema]:
-    return store_product_dao.get_all_store_products(store_id)
+@store_product_router.get(BASE + "/store/{store_id}", response_model=list[StoreProductSchema])
+def get_all_store_products_of_store(organization_id: UUID, store_id: UUID) -> list[StoreProductSchema]:
+    store = store_dao.get_store_by_id(organization_id, store_id)
+    if store is None:
+        raise ForbiddenException()
+
+    return store_product_dao.get_all_store_products_of_store(store_id)
+
+
+@store_product_router.get(BASE + "/product/{product_id}", response_model=list[StoreProductSchema])
+def get_all_store_products_of_product(organization_id: UUID, product_id: UUID) -> list[StoreProductSchema]:
+    product = product_dao.get_product(product_id)
+    if product.org_id != organization_id:
+        raise ForbiddenException()
+
+    return store_product_dao.get_all_store_products_of_products(product_id)
 
 
 @store_product_router.put(BASE + "/{store_id}/{product_id}", response_model=StoreProductSchema)
