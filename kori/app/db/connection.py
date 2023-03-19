10c3683a2ec7cb8c93be3bd54bf777b05ed26629
baxx <susanth1503@gmail.com>
2023-03-19 17:30:51 +0530

    change: db engine settings

--- kori/app/db/connection.py
+++ kori/app/db/connection.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import Session
 
 class DbConnector:
     def __init__(self, database_uri: str):
-        self.engine: Engine = create_engine(database_uri)
+        self.engine: Engine = create_engine(database_uri, pool_recycle=60, pool_size=20)
 
     def get_session(self) -> Session:
         session = Session(autocommit=False, autoflush=False, bind=self.engine)
